9208eb249556d5710fc0f8f5e8b606649d9a3aaf
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2023-01-17 17:00:18 +0200

    Added Bootstrap functionality

--- ecommerce_app/src/App.js
+++ ecommerce_app/src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -14,7 +16,6 @@ function App() {
       <div className="App">
         <Header/>
         <ProductList />
-        <Checkout />
       </div>
     </Provider>
   );
